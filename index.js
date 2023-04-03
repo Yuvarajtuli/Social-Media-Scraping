9c5345e9881645b021ec5c68349343a366837342
Sanchit Tewari <46245135+sanchitttt@users.noreply.github.com>
2023-04-03 21:33:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 const express = require('express');
-
 const { getQuoraData } = require('./controllers/main.controller');
 const verifyQuoraBody = require('./validations/quora.validation');
+const chromium = require('chromium');
+const {execFile} = require('child_process');
 const app = express();
-
 const PORT = process.env.PORT
 
 app.use(express.json());
 
+
+execFile(chromium.path, ['https://google.com'], err => {
+	console.log('Hello Google!');
+});
 
 app.get('/crawl/quora', verifyQuoraBody, getQuoraData);
 
